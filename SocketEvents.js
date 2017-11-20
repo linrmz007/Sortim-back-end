f975e74bdd7687a284d659e866b90eec7f9307ac
MarkyMark <mortaldyke3@gmail.com>
2017-11-20 19:11:22 +0100

    update before socket

---
+++ SocketEvents.js
@@ -0,0 +1,7 @@
+module.exports = (io) => {
+  io.on('connection', (socket) => {
+    socket.on('SOCKET__CONNECT', async (data) => {
+      
+    })
+  })
+}
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 'use strict';
-
+//express
 const express = require('express');
 const app = express();
+//socketIO
+const Server = require('http').Server;
+const server = Server(app);
+const io = require('socket.io')(server);
+const socketEvents = require('./socketEvents');
+//express
 const bodyParser = require('body-parser');
 const router = require('./router');
 const cors = require('cors');
@@ -12,6 +18,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 app.use(cors(corsOptions))
 app.use(router);
+
+socketEvents(io);
 app.listen(3001, function () {
     console.log('server running at localhost:3001');
   })
